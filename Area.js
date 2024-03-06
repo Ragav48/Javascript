506832dbb364a43a279996a4914a684528e811e8
Raghavendhiran N <nragavendran362@gmail.com>
2024-03-06 18:24:19 +0530

    New programs

---
+++ Area.js
@@ -0,0 +1,6 @@
+const basevalue = prompt("enter the basevalue:");
+const heightvalue = prompt("enter the heightvalue:");
+
+const area = (basevalue * heightvalue) / 2;
+
+console.log(area);
---
+++ Maximum.js
@@ -0,0 +1,11 @@
+const number1 = 4;
+const number2 = -9;
+const number3 = "jev";
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(result3);
+console.log(result2);
+console.log(result1);
